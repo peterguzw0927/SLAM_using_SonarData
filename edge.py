7d53df1865d714388aff8ed269626b647fda5d12
LuciaMartinezRuiz <123204798+LuciaMartinezRuiz@users.noreply.github.com>
2024-01-30 17:16:29 -0500

    edgeLeftBright.py

--- edge.py
+++ edge.py
@@ -1,45 +1,62 @@
 import cv2
+import numpy as np
 
 # Leer la imagen original
-img = cv2.imread('20190122_SCOUT.tif')
-
-# Display original image
-cv2.imshow('Original', img)
+img = cv2.imread('190122F0005.jpg')
+cv2.imshow('Image', img)
 cv2.waitKey(0)
 
-# Convertir a escala de grises
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Definir las coordenadas de la región de interés (ROI) centrada
+height, width = img.shape[:2]
+roi_x = width // 3
+roi_y = height // 15
+roi_width = width // 1
+roi_height = height // 1
 
-# Ajustar los niveles de contraste para hacer la escala de grises más exagerada
-alpha = 2  # Factor de escala
-beta = 50    # Factor de desplazamiento
-exaggerated_gray = cv2.convertScaleAbs(img_gray, alpha=alpha, beta=beta)
+# Crear la región de interés (ROI)
+roi = img[roi_y:roi_y+roi_height, roi_x:roi_x+roi_width]
+imgg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Mostrar la imagen original y la imagen con escala de grises exagerada
-cv2.imshow('Original Grayscale', img_gray)
-cv2.imshow('Exaggerated Grayscale', exaggerated_gray)
+
+# Ajustar los niveles de brillo en la parte izquierda de la imagen
+alpha_non_roi = 4.0  # You can adjust this value according to your preference
+beta_non_roi = 50    # You can adjust this value according to your preference
+
+# Aplicar el ajuste de brillo a la parte izquierda de la imagen
+imgg[:, :roi_x] = cv2.convertScaleAbs(imgg[:, :roi_x], alpha=alpha_non_roi, beta=beta_non_roi)
+cv2.imshow('Img gray left part only changed', imgg)
 cv2.waitKey(0)
 
-# Blur the exaggerated grayscale image for better edge detection
-img_blur = cv2.GaussianBlur(exaggerated_gray, (7,7), 0)
+
+# Ajustar los niveles de contraste y brillo en la imagen en escala de grises
+alpha = 6.0
+beta = 90
+high_contrast_result = cv2.convertScaleAbs(imgg, alpha=alpha, beta=beta)
+cv2.imshow('Img high contrast', high_contrast_result)
+cv2.waitKey(0)
+
+
+# Blur the high contrast grayscale image for better edge detection
+img_blur = cv2.GaussianBlur(high_contrast_result, (7, 7), 0)
 
 # Sobel Edge Detection
-sobelx = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=5)
-sobely = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=0, dy=1, ksize=5)
-sobelxy = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5)
+sobelx_result = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=5)
+sobely_result = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=0, dy=1, ksize=5)
+sobelxy_result = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5)
 
 # Display Sobel Edge Detection Images
-cv2.imshow('Sobel X', sobelx)
+cv2.imshow('Sobel X en Toda la Imagen', sobelx_result)
 cv2.waitKey(0)
-cv2.imshow('Sobel Y', sobely)
+cv2.imshow('Sobel Y en Toda la Imagen', sobely_result)
 cv2.waitKey(0)
-cv2.imshow('Sobel X Y using Sobel() function', sobelxy)
+cv2.imshow('Sobel X Y en Toda la Imagen usando Sobel()', sobelxy_result)
 cv2.waitKey(0)
 
 # Canny Edge Detection
-edges = cv2.Canny(image=img_blur, threshold1=100, threshold2=200)
+edges_result = cv2.Canny(image=img_blur, threshold1=100, threshold2=200)
+
 # Display Canny Edge Detection Image
-cv2.imshow('Canny Edge Detection', edges)
+cv2.imshow('Canny Edge Detection en Toda la Imagen', edges_result)
 cv2.waitKey(0)
 
 cv2.destroyAllWindows()
