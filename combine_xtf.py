ea228716923424c0e196c3726c19fececf9f7d54
Lydia <ccjs@bu.edu>
2024-02-21 15:43:17 -0500

    Improved efficiency
    Pings are now sorted as they are added to a heap rather than after being added to a list to improve the overall time complexity

--- combine_xtf.py
+++ combine_xtf.py
@@ -1,35 +1,36 @@
 import datetime
+import heapq
 import pyxtf
 import sys
 import os
+
+# Get the unix timestamp of a packet
+def get_packet_timestamp(x):
+    return datetime.datetime(x.Year, x.Month, x.Day, x.Hour, x.Minute, x.Second).timestamp()
+
+# Compare xtf packets by their unix timestamp
+setattr(pyxtf.XTFPacket, "__lt__", lambda self, other: get_packet_timestamp(self) <= get_packet_timestamp(other))
+
+
 
 if __name__ == '__main__':
     scan_folder = sys.argv[1] # Directory of XTF sonar files to merge
     
     # Get a list of xtf files
     file_list = [f for f in os.listdir() if os.path.isfile(f) and f.endswith('.xtf')] 
-    print(file_list)
+    #print(file_list)
     
     pings = []
-    packet_list = []
-    
-    # Read all sonar packets and store them in a single list
+    # Read all sonar packets and store them in a heap
     for file in file_list:
         (file_header, packets) = pyxtf.xtf_read(file)
         
-        #header_list.append(file_header)
-        packet_list.append(packets)
-
         for packet in packets[pyxtf.XTFHeaderType.sonar]:
-            pings.append(packet)
-    
+            heapq.heappush(pings,packet)
 
-    # Sort the list of packets by their timestamp
-    pings.sort(key=lambda x: datetime.datetime(x.Year, x.Month, x.Day, x.Hour, x.Minute, x.Second).timestamp(), reverse=False)
-
-
-    for p in pings:
-        print(p)
-        #print(p.PingNumber, p.data)
-        #print(p.Year, p.Month, p.Day, p.Hour, p.Minute, p.Second, p.PingNumber)
+    # Print the heap in order 
+    for count in range(len(pings)):
+        ping = heapq.heappop(pings)
+        print(ping)
+        #print(ping.Year, ping.Month, ping.Day, ping.Hour, ping.Minute, ping.Second, ping.PingNumber)
     #print(pings[0])
