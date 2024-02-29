d7c9a3b4f8b1e516d88669cab09a570e9715c48e
Lydia <ccjs@bu.edu>
2024-02-29 15:11:41 -0500

    Update combine_xtf.py

--- combine_xtf.py
+++ combine_xtf.py
@@ -11,26 +11,52 @@ def get_packet_timestamp(x):
 # Compare xtf packets by their unix timestamp
 setattr(pyxtf.XTFPacket, "__lt__", lambda self, other: get_packet_timestamp(self) <= get_packet_timestamp(other))
 
-
-
 if __name__ == '__main__':
     scan_folder = sys.argv[1] # Directory of XTF sonar files to merge
-    
+    output_file = sys.argv[2]
     # Get a list of xtf files
-    file_list = [f for f in os.listdir() if os.path.isfile(f) and f.endswith('.xtf')] 
+    file_list = [f for f in os.listdir() if os.path.isfile(f) and f.endswith('.xtf') and f != output_file] 
     #print(file_list)
     
     pings = []
-    # Read all sonar packets and store them in a heap
+    header_list = []
+    # Read each file
     for file in file_list:
         (file_header, packets) = pyxtf.xtf_read(file)
-        
+        header_list.append(file_header)
+        # Read add all sonar packets to a heap
         for packet in packets[pyxtf.XTFHeaderType.sonar]:
             heapq.heappush(pings,packet)
-
-    # Print the heap in order 
-    for count in range(len(pings)):
-        ping = heapq.heappop(pings)
-        print(ping)
+    
+    fh = pyxtf.XTFFileHeader()
+    fh.SonarName = b'TestWriter'
+    fh.SonarType = pyxtf.XTFSonarType.unknown1
+    fh.NavUnits = pyxtf.XTFNavUnits.latlon.value
+    fh.NumberOfSonarChannels = 2
+    
+    # Port chaninfo
+    fh.ChanInfo[0].TypeOfChannel = pyxtf.XTFChannelType.port.value
+    fh.ChanInfo[0].SubChannelNumber = 0
+    fh.ChanInfo[0].BytesPerSample = max(header_list, key=lambda x: x.ChanInfo[0].BytesPerSample).ChanInfo[0].BytesPerSample
+    fh.ChanInfo[0].SampleFormat = pyxtf.XTFSampleFormat.byte.value
+    # Stbd chaninfo
+    fh.ChanInfo[1].TypeOfChannel = pyxtf.XTFChannelType.stbd.value
+    fh.ChanInfo[1].SubChannelNumber = 1
+    fh.ChanInfo[1].BytesPerSample = max(header_list, key=lambda x: x.ChanInfo[1].BytesPerSample).ChanInfo[1].BytesPerSample
+    fh.ChanInfo[1].SampleFormat = pyxtf.XTFSampleFormat.byte.value
+    
+    # Write the new pings to the output file
+    with open(output_file, 'wb') as f:
+        # Write file header
+        f.write(fh.to_bytes())
+        for count in range(len(pings)):
+            ping = heapq.heappop(pings)
+            f.write(ping.to_bytes())
+            #print(ping)
+     # Try reading the created file
+    #(file_header, packets) = pyxtf.xtf_read(output_file)
+    
+    # Print the heap in order
         #print(ping.Year, ping.Month, ping.Day, ping.Hour, ping.Minute, ping.Second, ping.PingNumber)
-    #print(pings[0])
+
+   
