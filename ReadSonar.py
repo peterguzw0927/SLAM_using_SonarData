077b064c956cdf1bd2eab92eff1d91c34c5bff7d
hxl1236 <130591044+hxl1236@users.noreply.github.com>
2024-04-11 17:06:24 -0400

    Update ReadSonar.py

--- ReadSonar.py
+++ ReadSonar.py
@@ -2,15 +2,32 @@ import pyxtf
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import cv2
 
 def plot_xtf(file_path):
     # Read the XTF file
     (file_header, packets) = pyxtf.xtf_read(file_path)
+    # print('The following (supported) packets are present (XTFHeaderType:count): \n\t' +
+    #   str([key.name + ':{}'.format(len(v)) for key, v in packets.items()]))
+    packet_sizes = {key.name: len(value) for key, value in packets.items()}
 
     # Get the sonar channel data
     sonar_channel_data = packets[pyxtf.XTFHeaderType.sonar]
-
     print(sonar_channel_data[0])
+
+    # Initialize arrays to store the data
+    longitude = []
+    latitude = []
+    depth = []
+    heading = []
+
+    # Iterate through each ping and extract the data
+    for ping in sonar_channel_data:
+        # Extract sensor coordinates and heading
+        longitude.append(ping.SensorXcoordinate)
+        latitude.append(ping.SensorYcoordinate)
+        depth.append(ping.SensorDepth)
+        heading.append(ping.SensorHeading)
 
     # Concatenate sonar channel data
     np_chan1 = pyxtf.concatenate_channel(sonar_channel_data, file_header=file_header, channel=0, weighted=True)
@@ -21,7 +38,7 @@ def plot_xtf(file_path):
     np_chan1.clip(0, upper_limit-1, out=np_chan1)
     np_chan2.clip(0, upper_limit-1, out=np_chan2)
 
-    # Apply logarithmic transformation
+    # # # Apply logarithmic transformation
     np_chan1 = np.log10(np_chan1 + 0.0001)
     np_chan2 = np.log10(np_chan2 + 0.0001)
 
@@ -33,10 +50,16 @@ def plot_xtf(file_path):
 
     # Plot the waterfall view
     merged_image = np.hstack((np_chan1, np_chan2))
-    plt.figure(figsize=(12, 6))
-    plt.imshow(merged_image, cmap='gray')
-    plt.axis('off')  # Turn off axes
-    plt.show()
+    # plt.figure(figsize=(12, 6))
+    # plt.imshow(merged_image, cmap='gray')
+    # plt.axis('off')  # Turn off axes
+    # plt.show()
 
-    return merged_image
+    img = ((merged_image / 65536.0) * 255.0).astype(np.uint8) #Converting to 8 bit
+    # #print(img.shape)
+    # print(img)
+    # cv2.imshow('img', img)
+    # cv2.waitKey(0)
+    
+    return merged_image, longitude, latitude, depth, heading
 
